2a83932d91c401ebd2140ec3f4040857ea181e0b
levant47 <levant.ba@gmail.com>
2021-04-29 20:12:27 +0300

    Create 'DateTimeExtensions'

---
+++ Console/DateTimeExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Globalization;
+
+namespace IPZLabsVarCinema
+{
+    public static class DateTimeExtensions
+    {
+        // https://stackoverflow.com/questions/11154673/get-the-correct-week-number-of-a-given-date
+        // This presumes that weeks start with Monday.
+        // Week 1 is the 1st week of the year with a Thursday in it.
+        public static int Week(this DateTime time)
+        {
+            // Seriously cheat.  If its Monday, Tuesday or Wednesday, then it'll 
+            // be the same week# as whatever Thursday, Friday or Saturday are,
+            // and we always get those right
+            var day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+            {
+                time = time.AddDays(3);
+            }
+
+            // Return the week of our adjusted day
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+        } 
+
+    }
+}
