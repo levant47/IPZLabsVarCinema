36fccd104b365f73cfa911c86a9a7a5e6a6ec674
levant47 <levant.ba@gmail.com>
2021-05-02 18:06:35 +0300

    Create 'MovieDto'

---
+++ DataAccess/Dtos/MovieDto.cs
@@ -0,0 +1,11 @@
+namespace IPZLabsVarCinema
+{
+    public record MovieDto
+    (
+        int Id,
+        string Name,
+        int Year,
+        string Description,
+        string Poster
+    );
+}
