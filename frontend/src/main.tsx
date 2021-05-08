05267c4b5f6dec1234edf146ef5d9968241b6339
levant47 <levant.ba@gmail.com>
2021-05-08 11:37:33 +0300

    Add routing to 'Main'

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,20 @@
 import * as React from "react";
+import { Contacts } from "./contacts";
 import { Header } from "./header";
+import { LogIn } from "./login";
+import { Movies } from "./movies";
 
-export const Main = () => (
-    <>
-        <Header />
-    </>
-);
+export const Main = () => {
+    const route = location.pathname;
+    return (
+        <>
+            <Header />
+            {(route === "/" || route === "/movies") &&
+                <Movies />}
+            {route === "/login" &&
+                <LogIn />}
+            {route === "/contacts" &&
+                <Contacts />}
+        </>
+    );
+};
