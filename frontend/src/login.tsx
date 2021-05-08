6fcd27e94c1d5ea11d45c3ca15c7da3d91d9a5a5
levant47 <levant.ba@gmail.com>
2021-05-08 11:34:06 +0300

    Create 'LogIn'

---
+++ frontend/src/login.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+export const LogIn = () => (
+    <main>
+        <form>
+            <div>
+                <label>Email:</label>
+                <input type="text" placeholder="email" />
+            </div>
+
+            <div>
+                <label>Password:</label>
+                <input type="password" />
+            </div>
+        </form>
+    </main>
+);
