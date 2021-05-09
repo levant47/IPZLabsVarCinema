ea8a44dd434e24b54fe0574436efbd3263060470
levant47 <levant.ba@gmail.com>
2021-05-09 15:02:48 +0300

    Delete 'math.ts'

--- frontend/src/utils/math.ts
+++
@@ -1,7 +0,0 @@
-import { ℕ } from "./types";
-
-// [from; to)
-export const randomReal = (from = 0, to: number): number => Math.random() * (to - from) + from;
-
-// [from; to)
-export const randomInt = (from = 0, to: ℕ): ℕ => Math.floor(randomReal(from, to));
