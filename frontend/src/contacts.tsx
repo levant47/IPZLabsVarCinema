740bbcba8f4c7f2b2e5ddd83b665fda5306d78aa
levant47 <levant.ba@gmail.com>
2021-05-09 19:55:40 +0300

    Contacts

--- frontend/src/contacts.tsx
+++ frontend/src/contacts.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 
 export const Contacts = () => (
-    <main>
-        <p>Phone number</p>
-        <p>Email</p>
-        <p>Office address</p>
+    <main style={{margin: "0 20vw"}}>
+        <p>Phone number: 414-407-5345</p>
+        <p>Email: cinema@cinema.com</p>
+        <p>Office address: 705 Hillcrest Ave. Rolling Meadows, IL 60008</p>
     </main>
 );
