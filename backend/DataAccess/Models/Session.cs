211335d2728a4bfea2fb0029018c8debc0a1047a
levant47 <levant.ba@gmail.com>
2021-05-09 13:38:51 +0300

    Remove resource fields from 'Session'

--- backend/DataAccess/Models/Session.cs
+++ backend/DataAccess/Models/Session.cs
@@ -7,12 +7,6 @@ namespace IPZLabsVarCinema
         int Id,
         int MovieId,
         int HallId,
-        DateTime StartTime,
-        decimal PopcornAmount,
-        int Glasses3DAmount,
-        decimal DrinksAmount,
-        bool CleaningFinished,
-        bool LightingSetup,
-        bool CameraChecked
+        DateTime StartTime
     );
 }
